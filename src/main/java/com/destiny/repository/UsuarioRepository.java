1224be63d837a90980d859ac565ed11d39aa15dc
Cleiton.Silva <cleiton_ribeiro@live.com>
2023-08-28 07:21:55 -0300

    feat/ adicionando novo metodo de busca e suas dependencias

--- src/main/java/com/destiny/repository/UsuarioRepository.java
+++ src/main/java/com/destiny/repository/UsuarioRepository.java
@@ -1,31 +1,45 @@
 package com.destiny.repository;
 
+import com.destiny.model.StatusConta;
+import com.destiny.model.TipoConta;
 import com.destiny.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
-
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
 
     Usuario findByEmail(String email);
     Usuario findByCpf(String cpf);
     boolean existsByEmailAndIdNot(String email, Long id);
     boolean existsByCpfAndIdNot(String cpf, Long id);
-
+    @Query("SELECT u FROM Usuario u WHERE LOWER(u.nome) LIKE LOWER(CONCAT('%', :nome, '%'))")
+    List<UsuarioResumo> findByNomeContainingIgnoreCase(@Param("nome") String nome);
     public interface UsuarioResumo {
         Long getId();
-        String getNome();
         String getCpf();
         String getEmail();
-        Byte statusConta();
-        int tipoConta();
+        String getNome();
+        StatusConta getStatusConta();
+        TipoConta getTipoConta();
 
     }
 
-    @Query("SELECT new map(u.id as id, u.nome as nome, u.email as email, u.tipoConta as tipoConta, u.statusConta as statusConta ) FROM Usuario u")
-    List<Map<String, Object>> findAllCustom();
+    @Query("select u.id as id, u.cpf as cpf, u.email as email, u.nome as nome, u.statusConta as statusConta, u.tipoConta as tipoConta from Usuario u")
+    List<UsuarioResumo> findAllCustom();
+
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Usuario u SET u.statusConta = ?1 WHERE u.id = ?2")
+    void updateStatusConta(StatusConta statusConta, Long id);
+
+
 
 
 }
