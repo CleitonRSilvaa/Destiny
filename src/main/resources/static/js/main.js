614560758ef46860239f76767e41f2e7120ed476
Cleiton Silva <cleiton_ribeiro@live.com>
2023-08-28 15:32:46 -0300

    atualizando arquivos front

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,247 +1,275 @@
 $(document).ready(function () {
-    $("#cpf, #editCpf").mask("000.000.000-00");
+  $("#cpf, #editCpf").mask("000.000.000-00");
 });
-
-const BASE_URL = "http://localhost:8080";
-
-const FormManager = {
-    regexNumero(dado) {
-        return dado.replace(/[^\d]+/g, "");
+const BASE_URL = "http://localhost:8080",
+  FormManager = {
+    regexNumero: (e) => e.replace(/[^\d]+/g, ""),
+    isValidEmail: (e) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e),
+    showAlert(e, t) {
+      (e.textContent = t), (e.style.display = "block");
     },
-
-    isValidEmail(email) {
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(email);
+    hideAlert(e) {
+      (e.textContent = ""), (e.style.display = "none");
     },
-
-    showAlert(alert, message) {
-        alert.textContent = message;
-        alert.style.display = "block";
+    clearForm(e, t, a = {}) {
+      let i = document.getElementById(t);
+      e.forEach((e) => $(`#${e}`).val(a[e] || "")), this.hideAlert(i);
     },
-
-    hideAlert(alert) {
-        alert.textContent = "";
-        alert.style.display = "none";
+    senhasSaoIguais(e, t, a) {
+      let i = document.getElementById(e).value,
+        o = document.getElementById(t).value;
+      return i && o
+        ? i !== o
+          ? (document.getElementById(e).classList.add("alert", "alert-danger"),
+            document.getElementById(t).classList.add("alert", "alert-danger"),
+            !1)
+          : (document
+              .getElementById(e)
+              .classList.remove("alert", "alert-danger"),
+            document
+              .getElementById(t)
+              .classList.remove("alert", "alert-danger"),
+            !0)
+        : (this.showAlert(a, "As senhas s\xe3o obrigat\xf3rias!"), !1);
     },
-
-    clearForm(fields, alertId, defaultValues = {}) {
-        const alert = document.getElementById(alertId);
-        fields.forEach((field) => $(`#${field}`).val(defaultValues[field] || ""));
-        this.hideAlert(alert);
+    validateForm(e, t, a) {
+      let i = document.getElementById(t),
+        o = document.getElementById("editSenha")
+          ? document.getElementById("editSenha").value.trim()
+          : "",
+        n = document.getElementById("editSenhaConfime")
+          ? document.getElementById("editSenhaConfime").value.trim()
+          : "",
+        s = document.getElementById("senha")
+          ? document.getElementById("senha").value.trim()
+          : "",
+        r = document.getElementById("senhaConfime")
+          ? document.getElementById("senhaConfime").value.trim()
+          : "";
+      if ((o && !n) || (!o && n))
+        return (
+          this.showAlert(
+            i,
+            "Ambos os campos de senha de edi\xe7\xe3o devem ser preenchidos ou ambos devem ser deixados em branco!"
+          ),
+          !1
+        );
+      if (a && (!s || !r))
+        return (
+          this.showAlert(i, "Os campos de senha s\xe3o obrigat\xf3rios!"), !1
+        );
+      for (let d of e) {
+        let l = $(`#${d}`).val().trim();
+        if (("email" === d || "editEmail" === d) && !this.isValidEmail(l))
+          return this.showAlert(i, "Email inv\xe1lido!"), !1;
+        if (!l && !["editSenha", "editSenhaConfime"].includes(d))
+          return (
+            this.showAlert(i, "Todos os campos s\xe3o obrigat\xf3rios!"), !1
+          );
+      }
+      return (o || n) &&
+        !this.senhasSaoIguais("editSenha", "editSenhaConfime", i)
+        ? (this.showAlert(i, "As senhas de edi\xe7\xe3o n\xe3o coincidem!"), !1)
+        : a && !this.senhasSaoIguais("senha", "senhaConfime", i)
+        ? (this.showAlert(i, "As senhas n\xe3o coincidem!"), !1)
+        : (this.hideAlert(i), !0);
     },
-
-    senhasSaoIguais(idSenha, idSenhaConfime, alert) {
-        const senha = document.getElementById(idSenha).value;
-        const senhaConfime = document.getElementById(idSenhaConfime).value;
-
-        if (!senha || !senhaConfime) {
-            this.showAlert(alert, "As senhas são obrigatórias!");
-            return false;
-        }
-
-        if (senha !== senhaConfime) {
-            document.getElementById(idSenha).classList.add("alert", "alert-danger");
-            document.getElementById(idSenhaConfime).classList.add("alert", "alert-danger");
-            return false;
-        } else {
-            document.getElementById(idSenha).classList.remove("alert", "alert-danger");
-            document.getElementById(idSenhaConfime).classList.remove("alert", "alert-danger");
-            return true;
-        }
-    },
-
-    validateForm(fields, alertId, isCreating) {
-        const alert = document.getElementById(alertId);
-
-        // Valores dos campos de senha e edição de senha
-        const editSenhaValue = document.getElementById('editSenha') ? document.getElementById('editSenha').value.trim() : '';
-        const editSenhaConfimeValue = document.getElementById('editSenhaConfime') ? document.getElementById('editSenhaConfime').value.trim() : '';
-        const senhaValue = document.getElementById('senha') ? document.getElementById('senha').value.trim() : '';
-        const senhaConfimeValue = document.getElementById('senhaConfime') ? document.getElementById('senhaConfime').value.trim() : '';
-
-        // Se apenas um dos campos de senha de edição estiver preenchido
-        if ((editSenhaValue && !editSenhaConfimeValue) || (!editSenhaValue && editSenhaConfimeValue)) {
-            this.showAlert(alert, "Ambos os campos de senha de edição devem ser preenchidos ou ambos devem ser deixados em branco!");
-            return false;
-        }
-
-
-        // Se estamos criando um novo usuário e os campos senha ou senhaConfime estiverem vazios
-        if (isCreating && (!senhaValue || !senhaConfimeValue)) {
-            this.showAlert(alert, "Os campos de senha são obrigatórios!");
-            return false;
-        }
-
-        for (let field of fields) {
-            let value = $(`#${field}`).val().trim();
-
-            if ((field === "email" || field === "editEmail") && !this.isValidEmail(value)) {
-                this.showAlert(alert, "Email inválido!");
-                return false;
-            }
-
-            // Ignoramos a validação de obrigatoriedade para os campos editSenha e editSenhaConfime, pois já tratamos acima
-            if (!value && !["editSenha", "editSenhaConfime"].includes(field)) {
-                this.showAlert(alert, "Todos os campos são obrigatórios!");
-                return false;
-            }
-        }
-
-        if (editSenhaValue || editSenhaConfimeValue) {
-            if (!this.senhasSaoIguais('editSenha', 'editSenhaConfime', alert)) {
-                this.showAlert(alert, "As senhas de edição não coincidem!");
-                return false;
-            }
-        }
-
-        if (isCreating && !(this.senhasSaoIguais('senha', 'senhaConfime', alert))) {
-            this.showAlert(alert, "As senhas não coincidem!");
-            return false;
-        }
-
-        this.hideAlert(alert);
-        return true;
-    }
-
-
-};
-
-// Event Listeners para fechar modais
-document.getElementById("staticBackdrop").addEventListener("hidden.bs.modal", function () {
-    FormManager.clearForm(["nome", "email", "senha", "cpf", "senhaConfime", "tipoConta"], "usuarioAlert", { tipoConta: "ESTOQUISTA" });
-});
-
-document.getElementById("modalEdicao").addEventListener("hidden.bs.modal", function () {
-    FormManager.clearForm(["editId", "editNome", "editEmail", "editCpf", "editSenha", "editSenhaConfime", "editStatus", "editTipoConta"], "edicaoUsuarioAlert");
-});
-
-function openEditModal(button) {
-
-  const tr = button.closest("tr");
-
-  const dataAttrs = ["id", "nome", "email", "cpf", "tipoConta", "status"];
-  dataAttrs.forEach((attr) => {
-    const elementId = `edit${attr.charAt(0).toUpperCase() + attr.slice(1)}`;
-    const element = document.getElementById(elementId);
-
-    if (!element) {
-      console.error(`Element with ID ${elementId} not found!`);
+  };
+function openEditModal(e) {
+  let t = e.closest("tr");
+  ["id", "nome", "email", "cpf", "tipoConta", "status"].forEach((e) => {
+    let a = `edit${e.charAt(0).toUpperCase() + e.slice(1)}`,
+      i = document.getElementById(a);
+    if (!i) {
+      console.error(`Element with ID ${a} not found!`);
       return;
     }
-
-    const dataValue = tr.getAttribute(`data-${attr}`);
-
-    if (dataValue === null) {
-      console.error(`Data attribute data-${attr} not found on the row!`);
+    let o = t.getAttribute(`data-${e}`);
+    if (null === o) {
+      console.error(`Data attribute data-${e} not found on the row!`);
       return;
     }
-
-    element.value = dataValue;
+    i.value = o;
   });
-
-  const modal = new bootstrap.Modal(document.getElementById("modalEdicao"));
-  modal.show();
+  let a = new bootstrap.Modal(document.getElementById("modalEdicao"));
+  a.show();
 }
-
-
-function makeRequest(type, url, data, successMessage) {
- console.log(data);
+function makeRequest(e, t, a, i) {
+  console.log(a),
     $.ajax({
-        type: type,
-        url: `${BASE_URL}${url}`,  // Usando a BASE_URL como sugerido anteriormente
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-
-        success: function (response) {
-            if ([200, 201].includes(response.status) && response.message === "sucess") {
-                Swal.fire({
-                    position: "top-end",
-                    icon: "success",
-                    title: successMessage,
-                    showConfirmButton: false,
-                    timer: 1900,
-                });
-                setTimeout(() => location.reload(), 1900);
-            } else {
-                Swal.fire({
-                    icon: "error",
-                    title: "Erro",
-                    text: "Resposta inesperada do servidor. Por favor, tente novamente.",
-                });
-            }
-        },
-        error: function (error) {
-            const response = error.responseJSON;
-            let errorMessage = response.message + "\n\n";
-            if (response.details) {
-                errorMessage += response.details
-                    .map((detail) => "- " + detail)
-                    .join("\n");
-            }
-
-            Swal.fire({
-                icon: "error",
-                title: "Erro",
-                text: errorMessage,
+      type: e,
+      url: `http://localhost:8080${t}`,
+      data: JSON.stringify(a),
+      contentType: "application/json; charset=utf-8",
+      success: function (e) {
+        [200, 201].includes(e.status) && "sucess" === e.message
+          ? (Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: i,
+              showConfirmButton: !1,
+              timer: 1900,
+            }),
+            setTimeout(() => location.reload(), 1900))
+          : Swal.fire({
+              icon: "error",
+              title: "Erro",
+              text: "Resposta inesperada do servidor. Por favor, tente novamente.",
             });
-        },
+      },
+      error: function (e) {
+        let t = e.responseJSON,
+          a = t.message + "\n\n";
+        t.details && (a += t.details.map((e) => "- " + e).join("\n")),
+          Swal.fire({ icon: "error", title: "Erro", text: a });
+      },
     });
 }
-
-function updateStatusConta(button) {
-    const tr = button.closest("tr");
-    const id = tr.getAttribute("data-id");
-    const status = tr.getAttribute("data-status");
-    const newStatus = status === "ATIVA" ? "INATIVA" : "ATIVA";
-    const data = { id, status: newStatus };
-
-    Swal.fire({
-        title: `${newStatus === "ATIVA" ? "Ativar" : "Inativar"} usuário?`,
-        showCancelButton: true,
-        confirmButtonText: "Sim",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            makeRequest("POST", "/usuario/updateStatus", data, "Status atualizado com sucesso!");
-        }
-    });
+function updateStatusConta(e) {
+  let t = e.closest("tr"),
+    a = t.getAttribute("data-id"),
+    i = t.getAttribute("data-status"),
+    o = "ATIVA" === i ? "INATIVA" : "ATIVA",
+    n = { id: a, status: o };
+  Swal.fire({
+    title: `${"ATIVA" === o ? "Ativar" : "Inativar"} usu\xe1rio?`,
+    showCancelButton: !0,
+    confirmButtonText: "Sim",
+  }).then((e) => {
+    e.isConfirmed &&
+      makeRequest(
+        "POST",
+        "/usuario/updateStatus",
+        n,
+        "Status atualizado com sucesso!"
+      );
+  });
 }
-
-
-function getFormData(fields, mapping = null) {
-    let data = {};
-    fields.forEach(field => {
-        const key = mapping && mapping[field] ? mapping[field] : field;
-
-        data[key] = $(`#${field}`).val().trim();
-
-        if (field.includes('cpf')) {
-            data[key] = FormManager.regexNumero(data[key]);  // Fixed method call
-        }
-    });
-    return data;
+function getFormData(e, t = null) {
+  let a = {};
+  return (
+    e.forEach((e) => {
+      let i = t && t[e] ? t[e] : e;
+      (a[i] = $(`#${e}`).val().trim()),
+        e.includes("cpf") && (a[i] = FormManager.regexNumero(a[i]));
+    }),
+    a
+  );
 }
-
 function submitForm() {
-    if (!FormManager.validateForm(["nome", "email", "senha","senhaConfime", "cpf", "tipoConta"], "usuarioAlert", true)) {
-        return;
-    }
-    const data = getFormData(["nome", "email", "senha", "cpf", "tipoConta"]);
-    makeRequest("POST", "/usuario/add", data, "Usuário adicionado com sucesso!");
+  if (
+    !FormManager.validateForm(
+      ["nome", "email", "senha", "senhaConfime", "cpf", "tipoConta"],
+      "usuarioAlert",
+      !0
+    )
+  )
+    return;
+  let e = getFormData(["nome", "email", "senha", "cpf", "tipoConta"]);
+  makeRequest("POST", "/usuario/add", e, "Usu\xe1rio adicionado com sucesso!");
 }
-
 function updateUsuario() {
-    if (!FormManager.validateForm(["editId", "editNome", "editEmail", "editCpf", "editSenha", "editTipoConta", "editStatus"], "edicaoUsuarioAlert", false)) {
-        return;
+  if (
+    !FormManager.validateForm(
+      [
+        "editId",
+        "editNome",
+        "editEmail",
+        "editCpf",
+        "editSenha",
+        "editTipoConta",
+        "editStatus",
+      ],
+      "edicaoUsuarioAlert",
+      !1
+    )
+  )
+    return;
+  let e = getFormData(
+    [
+      "editId",
+      "editNome",
+      "editEmail",
+      "editCpf",
+      "editSenha",
+      "editTipoConta",
+      "editStatus",
+    ],
+    {
+      editId: "id",
+      editNome: "nome",
+      editEmail: "email",
+      editCpf: "cpf",
+      editSenha: "senha",
+      editTipoConta: "tipoConta",
+      editStatus: "statusConta",
     }
-    const mapping = {
-        editId: "id",
-        editNome: "nome",
-        editEmail: "email",
-        editCpf: "cpf",
-        editSenha: "senha",
-        editTipoConta: "tipoConta",
-        editStatus: "statusConta"
-    };
-    const data = getFormData(["editId", "editNome", "editEmail", "editCpf", "editSenha", "editTipoConta", "editStatus"], mapping);
-    makeRequest("PUT", "/usuario/update", data, "Usuário atualizado com sucesso!");
+  );
+  makeRequest(
+    "PUT",
+    "/usuario/update",
+    e,
+    "Usu\xe1rio atualizado com sucesso!"
+  );
 }
+document
+  .getElementById("staticBackdrop")
+  .addEventListener("hidden.bs.modal", function () {
+    FormManager.clearForm(
+      ["nome", "email", "senha", "cpf", "senhaConfime", "tipoConta"],
+      "usuarioAlert",
+      { tipoConta: "ESTOQUISTA" }
+    );
+  }),
+  document
+    .getElementById("modalEdicao")
+    .addEventListener("hidden.bs.modal", function () {
+      FormManager.clearForm(
+        [
+          "editId",
+          "editNome",
+          "editEmail",
+          "editCpf",
+          "editSenha",
+          "editSenhaConfime",
+          "editStatus",
+          "editTipoConta",
+        ],
+        "edicaoUsuarioAlert"
+      );
+    });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var tabela = document.querySelector("tbody");
+  if (tabela.querySelectorAll("tr").length > 20) {
+    tabela.classList.add("scrollable");
+  }
+});
+
+$(document).ready(function () {
+  var $tabela = $("tbody");
+  if ($tabela.find("tr").length > 50) {
+    $tabela.addClass("scrollable");
+  }
+});
+
+
+// Definindo o endpoint
+const url = "http://localhost:5000/api/delsoftx/Login?matricula=1800&cpf=2560";
+
+// Usando a API fetch para fazer uma chamada GET
+fetch(url, {
+    method: 'GET', // especifica o método da requisição
+    headers: {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
+    }
+})
+.then(response => response.json()) // presumindo que a resposta seja um JSON
+.then(data => {
+    console.log(data); // aqui você tem o resultado da chamada
+})
+.catch(error => {
+    console.error('Erro ao fazer a requisição:', error);
+});
