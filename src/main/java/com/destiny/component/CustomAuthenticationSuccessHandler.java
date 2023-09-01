cf07f48f69dafac30984a3ddc94efe8706363e87
Cleiton Silva <cleiton_ribeiro@live.com>
2023-09-01 17:21:42 -0300

    update tela login

--- src/main/java/com/destiny/component/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/destiny/component/CustomAuthenticationSuccessHandler.java
@@ -14,30 +14,28 @@ import java.io.IOException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-
-
-
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     private RequestCache requestCache = new HttpSessionRequestCache();
 
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+            Authentication authentication) throws IOException, ServletException {
 
         SavedRequest savedRequest = requestCache.getRequest(request, response);
 
         if (savedRequest != null) {
             // Redireciona para a última página solicitada antes do login
-            response.sendRedirect(savedRequest.getRedirectUrl());
+            String redirectURL = determineTargetUrl(authentication);
+            response.sendRedirect(redirectURL);
         } else {
             // Se não houver uma página salva, use a lógica padrão
             String redirectURL = determineTargetUrl(authentication);
             response.sendRedirect(redirectURL);
         }
     }
+
     protected String determineTargetUrl(Authentication authentication) {
         Set<String> roles = authentication.getAuthorities().stream()
                 .map(r -> r.getAuthority())
@@ -48,7 +46,7 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
         } else if (roles.contains("ROLE_ESTOQUISTA")) {
             return "/estoque/dashboard";
         } else {
-            return "/home";  // para outros usuários ou default
+            return "/home"; // para outros usuários ou default
         }
     }
 }
