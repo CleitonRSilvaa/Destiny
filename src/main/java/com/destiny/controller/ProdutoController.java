68de46cac5c0966074e65f6a484661d8cac6874b
Cleiton.Silva <cleiton_ribeiro@live.com>
2023-09-08 16:41:26 -0300

    feat/implementando metodo de cadastro de imgs

--- src/main/java/com/destiny/controller/ProdutoController.java
+++ src/main/java/com/destiny/controller/ProdutoController.java
@@ -1,22 +1,57 @@
 package com.destiny.controller;
 
+import com.destiny.model.Imagem;
+import com.destiny.model.Produto;
+import com.destiny.repository.ImagemRepository;
+import com.destiny.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Controller()
 @RequestMapping("/produto")
 public class ProdutoController {
-    
-    @GetMapping("/listarProduto") 
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    @Autowired
+    private ImagemRepository imagemRepository;
+
+
+    @GetMapping("/listar")
     public String telaProduto(){
-        return "estoque/produtos.html"; // Não inclua a barra inicial e a extensão .html
+        List<Produto> produtoList = produtoRepository.findAll();
+        for (Produto p:produtoList) {
+            for (Imagem img:p.getImagens()) {
+                System.out.println(img.getCaminho());
+            }
+        }
+
+
+        return "admin/admin-menager_produtos";
     }
 
     @GetMapping("/addProduto")
     public String telaCadastro(Model model){
+
+
+
 
 
         model.getAttribute("nomeProduto");
@@ -30,6 +65,63 @@ public class ProdutoController {
     }
 
 
+    @PostMapping("/add")
+    @Transactional
+    public String cadastrarProduto(@RequestParam("nome") String nome, @RequestParam("imagem") MultipartFile[] imagens) {
+        if (imagens == null || imagens.length == 0) {
+            throw new IllegalArgumentException("Nome e imagens são requeridos");
+        }
+
+        Produto produto = new Produto();
+        produto.setNome(nome);
+        produtoRepository.save(produto);
+
+
+
+        for (MultipartFile imagem : imagens) {
+            System.out.println("entrou no for");
+            if (imagem != null && !imagem.isEmpty()) {
+                String nomeImagem =dataHoraStrg().concat(StringUtils.cleanPath(imagem.getOriginalFilename().replace(" ","_")));
+                String caminho = "imagens/produtos/" + nomeImagem;
+                Path path = Paths.get("src/main/resources/static/" + caminho);
+                try {
+                    Files.copy(imagem.getInputStream(), path,StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    System.out.println("Falha ao armazenar a imagem " + nomeImagem + e);
+                }
+
+                Imagem novaImagem = new Imagem();
+                novaImagem.setCaminho(caminho);
+                novaImagem.setProduto(produto);
+                System.out.println(novaImagem);
+                imagemRepository.save(novaImagem);
+            }else {
+                String nomeImagem = "default.jpg";
+                String caminho = "imagens/produtos/" + nomeImagem;
+                Path path = Paths.get("src/main/resources/static/" + caminho);
+                Imagem novaImagem = new Imagem();
+                novaImagem.setCaminho(caminho);
+                novaImagem.setProduto(produto);
+                imagemRepository.save(novaImagem);
+                break;
+            }
+        }
+
+
+
+        return "redirect:/produto/listar";
+    }
+
+
+
+
+
+    private String dataHoraStrg(){
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String dataHoraFormatada = agora.format(formato).concat("-".trim().toLowerCase()).replace(':','-');
+        return dataHoraFormatada;
+    }
 
 
 }
