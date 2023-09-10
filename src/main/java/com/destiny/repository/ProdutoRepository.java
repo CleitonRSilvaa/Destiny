9beeb09c3b3d8e84a227352d9fc70b3e2e038758
Cleiton.Silva <cleiton_ribeiro@live.com>
2023-09-09 21:48:43 -0300

    feat/ buscar produto por nome

--- src/main/java/com/destiny/repository/ProdutoRepository.java
+++ src/main/java/com/destiny/repository/ProdutoRepository.java
@@ -1,7 +1,12 @@
 package com.destiny.repository;
 
 import com.destiny.model.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Page<Produto> findByNomeContaining(String nome, Pageable pageable);
+
+
 }
