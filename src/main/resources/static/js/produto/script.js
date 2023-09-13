1ae2c0ea932fd06cab5c5960c2f7cbb0d6a8c014
Cleiton Silva <cleiton_ribeiro@live.com>
2023-09-13 15:48:22 -0300

    feat/telas preview produtos

---
+++ src/main/resources/static/js/produto/script.js
@@ -0,0 +1,17 @@
+const carousel = document.querySelector("#carouselMain");
+const thumbnails = document.querySelectorAll("#thumbnailContainer img");
+
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", () => {
+    const carouselInstance = bootstrap.Carousel.getInstance(carousel);
+    carouselInstance.to(index);
+
+    thumbnails.forEach((thumb) => thumb.classList.remove("active"));
+    thumbnail.classList.add("active");
+  });
+});
+
+carousel.addEventListener("slid.bs.carousel", (event) => {
+  thumbnails.forEach((thumb) => thumb.classList.remove("active"));
+  thumbnails[event.to].classList.add("active");
+});
