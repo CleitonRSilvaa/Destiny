d8b59d28e599e9f523a8d7ca4418e151ddd9fb7f
Cleiton.Silva <cleiton_ribeiro@live.com>
2023-10-11 21:00:47 -0300

    Fix(cadrastroCliente) validando dados pessoais

--- src/main/resources/static/js/cliente/main.js
+++ src/main/resources/static/js/cliente/main.js
@@ -101,10 +101,13 @@ function app() {
 
     setErrorMessage: (input, message) => {
       if (message) {
+        input.classList.remove("has-ok");
         input.classList.add("has-error");
       } else {
         input.classList.remove("has-error");
+        input.classList.add("has-ok");
       }
+
       const existingError = input.nextElementSibling;
 
       if (existingError && existingError.classList.contains("error-message")) {
@@ -116,6 +119,7 @@ function app() {
         input.insertAdjacentElement("afterend", p);
       }
     },
+
     validateStep1: function () {
       var name = document.getElementById("name");
       var email = document.getElementById("email");
@@ -125,25 +129,85 @@ function app() {
 
       var isValid = true;
 
+      regexNumeroCpf = (dado) => {
+        dado = dado.replace(/[^\d]+/g, "");
+        return dado;
+      };
+
+      validarCPF = (cpf) => {
+        cpf = regexNumeroCpf(cpf);
+
+        if (cpf.length !== 11 || /^(.)\1+$/.test(cpf)) {
+          return false;
+        }
+
+        var soma = 0;
+        var resto;
+
+        for (var i = 1; i <= 9; i++) {
+          soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        }
+
+        resto = (soma * 10) % 11;
+
+        if (resto === 10 || resto === 11) {
+          resto = 0;
+        }
+
+        if (resto !== parseInt(cpf.substring(9, 10))) {
+          return false;
+        }
+
+        soma = 0;
+
+        for (var j = 1; j <= 10; j++) {
+          soma += parseInt(cpf.substring(j - 1, j)) * (12 - j);
+        }
+
+        resto = (soma * 10) % 11;
+
+        if (resto === 10 || resto === 11) {
+          resto = 0;
+        }
+
+        if (resto !== parseInt(cpf.substring(10, 11))) {
+          return false;
+        }
+
+        return true;
+      };
+
+      var namePattern = /^(?:[A-Za-z]{3,}\s){1}[A-Za-z]{3,}$/;
+
       if (!name.value.trim()) {
         isValid = false;
         this.setErrorMessage(name, "Nome é obrigatório!", "red");
+      } else if (!namePattern.test(name.value.trim())) {
+        isValid = false;
+        this.setErrorMessage(
+          name,
+          "O nome deve conter duas palavras com no mínimo 3 letras cada!",
+          "red"
+        );
       } else {
-        this.setErrorMessage(name, "", "#86ffb5");
+        this.setErrorMessage(name, "");
       }
 
       if (!email.value.trim()) {
         isValid = false;
         this.setErrorMessage(email, "Email é obrigatório!", "red");
       } else {
-        this.setErrorMessage(email, "", "#86ffb5");
+        this.setErrorMessage(email, "");
       }
 
       if (!cpf.value.trim()) {
         isValid = false;
         this.setErrorMessage(cpf, "CPF é obrigatório!", "red");
+      } else if (!validarCPF(cpf.value.trim())) {
+        isValid = false;
+        this.setErrorMessage(cpf, "Por favor, digite um CPF válido!", "red");
       } else {
-        this.setErrorMessage(cpf, "", "#86ffb5");
+        this.setErrorMessage(cpf, "");
       }
 
       if (!birthdate.value.trim()) {
@@ -154,14 +218,38 @@ function app() {
           "red"
         );
       } else {
-        this.setErrorMessage(birthdate, "", "#86ffb5");
+        const inputDate = new Date(birthdate.value);
+        const today = new Date();
+        const eighteenYearsAgo = new Date(
+          today.getFullYear() - 18,
+          today.getMonth(),
+          today.getDate()
+        );
+
+        if (inputDate > today) {
+          isValid = false;
+          this.setErrorMessage(
+            birthdate,
+            "A data de nascimento não pode ser maior que a data atual!",
+            "red"
+          );
+        } else if (inputDate > eighteenYearsAgo) {
+          isValid = false;
+          this.setErrorMessage(
+            birthdate,
+            "Você deve ter pelo menos 18 anos de idade!",
+            "red"
+          );
+        } else {
+          this.setErrorMessage(birthdate, "");
+        }
       }
 
       if (!genero.value.trim()) {
         isValid = false;
         this.setErrorMessage(genero, "Gênero é obrigatório!", "red");
       } else {
-        this.setErrorMessage(genero, "", "#86ffb5");
+        this.setErrorMessage(genero, "");
       }
 
       return isValid;
