a1567df736d9975d998fdb3f1d99cfdd4f82147b
Cleiton.Silva <cleiton_ribeiro@live.com>
2023-11-05 20:55:37 -0300

    criando tela checkout  e carrinho de compras ajuste links

--- src/main/resources/static/js/carrinho.js
+++ src/main/resources/static/js/carrinho.js
@@ -1,10 +1,9 @@
 let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
 function adicionarAoCarrinho(btn) {
-
   console.log("Antes de desativar:", btn.disabled);
   btn.disabled = true;
-  
+
   const produtoId = btn.getAttribute("data-id");
   const nome =
     btn.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.getAttribute(
@@ -26,6 +25,12 @@ function adicionarAoCarrinho(btn) {
     );
   const quantidade = parseInt(document.getElementById("quantidade").value);
 
+  // Assumindo que o elemento img tem um ID 'imgPrincipal'
+  let imagem = document.getElementById("imgPrincipal");
+
+  // Pegar o valor do atributo 'src'
+  const img = imagem.getAttribute("src");
+
   let produto = {
     id: produtoId,
     nome: nome,
@@ -34,19 +39,68 @@ function adicionarAoCarrinho(btn) {
     descricao: descricao,
     quantidade: quantidade,
     subTotal: 0,
+    img: img,
   };
   console.log("Depois de desativar:", btn.disabled);
   adicionaAoCarrinhoLogic(produto, btn);
- }
+  btn.disabled = false;
+}
 
-function adicionaAoCarrinhoLogic(produto,btn) {
-  
+function removerItem(id) {
+  let itemExistente = carrinho.find((item) => item.id === String(id));
+  if (itemExistente) {
+    carrinho = carrinho.filter((item) => item.id !== String(id));
+  }
+  atualizaVisualizacaoCarrinho();
+  atualizaLocalStorage();
+  location.reload();
+}
+
+function removerItemModalConfirme() {
+  removerItem(document.getElementById("id-item-remove").value);
+}
+
+function aumentarQuantidade(id) {
+  let itemExistente = carrinho.find((item) => item.id === String(id));
+  if (itemExistente) {
+    itemExistente.quantidade += 1;
+    itemExistente.subTotal = itemExistente.quantidade * itemExistente.valor;
+  }
+  atualizaVisualizacaoCarrinho();
+  atualizaLocalStorage();
+  location.reload();
+}
+
+function diminuirQuantidade(id) {
+  let itemExistente = carrinho.find((item) => item.id === String(id));
+  if (itemExistente) {
+    if (itemExistente.quantidade == 1) {
+      toggleModal("popup-modal", String(id));
+      return;
+    }
+    itemExistente.quantidade -= 1;
+    itemExistente.subTotal = itemExistente.quantidade * itemExistente.valor;
+  }
+  atualizaVisualizacaoCarrinho();
+  atualizaLocalStorage();
+  location.reload();
+}
+
+function toggleModal(modalID, value) {
+  document.getElementById(modalID).classList.toggle("hidden");
+  document.getElementById("id-item-remove").value = value;
+  document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+  document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+}
+
+function adicionaAoCarrinhoLogic(produto, btn) {
   let itemExistente = carrinho.find((item) => item.id === produto.id);
 
   if (itemExistente) {
     itemExistente.quantidade += produto.quantidade;
     itemExistente.subTotal = itemExistente.quantidade * itemExistente.valor;
   } else {
+    produto.subTotal = produto.quantidade * produto.valor;
     carrinho.push(produto);
   }
 
@@ -56,7 +110,8 @@ function adicionaAoCarrinhoLogic(produto,btn) {
   // Por exemplo, mostrando um número indicando quantos itens estão no carrinho.
   atualizaVisualizacaoCarrinho();
 
-  atualizaLocalStorage(btn);
+  atualizaLocalStorage();
+  return;
 }
 
 function exibirPopup() {
@@ -65,26 +120,32 @@ function exibirPopup() {
 
   setTimeout(() => {
     popup.classList.add("hidden");
-  }, 3000);  // O popup será ocultado após 3 segundos
+  }, 3000); // O popup será ocultado após 3 segundos
+
+  return;
 }
 
-function atualizaLocalStorage(btn) {
-  btn.disabled = false;
+function atualizaLocalStorage() {
+  // btn.disabled = false;
   localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  return;
 }
 
 function atualizaVisualizacaoCarrinho() {
   console.info(carrinho);
-  const totalItens = carrinho.reduce(
-    (acc, produto) => acc + produto.quantidade,
-    0
-  );
+
+  const totalItens = carrinho.length;
+  // const totalItens = carrinho.reduce(
+  //   (acc, produto) => acc + produto.quantidade,
+  //   0
+  // );
   const totalValor = carrinho.reduce(
     (acc, produto) => acc + produto.quantidade * produto.valor,
     0
   );
 
   document.getElementById("carrinhoCount").textContent = totalItens;
+  return;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -97,12 +158,11 @@ function limpaCarrinho() {
   atualizaLocalStorage();
 }
 
-
 function exibirModalNotificacao() {
-  const modal = document.getElementById('modalNotification');
-  modal.classList.remove('hidden');
+  const modal = document.getElementById("modalNotification");
+  modal.classList.remove("hidden");
 
   setTimeout(() => {
-    modal.classList.add('hidden');
-  }, 1000);  // O modal será ocultado após 3 segundos
+    modal.classList.add("hidden");
+  }, 1000); // O modal será ocultado após 3 segundos
 }
---
+++ src/main/resources/static/js/cliente/checkout.js
@@ -0,0 +1,438 @@
+$(document).ready(function () {
+  $("#cep").mask("00000-000");
+  $("#card-no").mask("0000-0000-0000-0000");
+  $("#codigo-cvc").mask("000");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  addEventListeners();
+});
+
+let carrinho2 = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+let carrinhoContainer = document.getElementById("produto-list");
+
+// Itera sobre os itens do carrinho e cria a marcação HTML para cada um
+carrinho2.forEach((item) => {
+  let itemDiv = `
+  <div class="flex items-center mb-5">
+    <div class="w-1/6">
+    <img src="${item.img}" alt="${item.nome}"
+    class="w-full h-auto object-cover rounded">
+    </div>
+    <div class="w-6/6 pl-4">
+      <h2 class="text-lg font-semibold">${item.nome}</h2>
+      <p class="text-sm">Valor unitario: R$${item.valor.toFixed(2)}</p>
+      <p class="text-sm">Quantidade: ${item.quantidade}</p>
+      <p class="text-lg font-semibold">SubTotal: R$${item.subTotal.toFixed(
+        2
+      )}</p>
+    </div>
+  </div>
+        
+    `;
+  // Adiciona o itemDiv no carrinhoContainer
+  carrinhoContainer.insertAdjacentHTML("beforeend", itemDiv);
+});
+
+function toggleModal(modalID) {
+  document.getElementById(modalID).classList.toggle("hidden");
+  document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+  document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+}
+
+function togglePaymentMethod(method) {
+  const pagamentos = document.querySelectorAll(".payment-item");
+  var cardDetails = document.getElementById("card-details");
+  pagamentos.forEach((pagamento) => {
+    pagamento.classList.remove("selected");
+    if (pagamento.id === method) {
+      pagamento.classList.add("selected");
+    }
+  });
+
+  if (method === "payment-card") {
+    cardDetails.classList.remove("hidden");
+  } else {
+    cardDetails.classList.add("hidden");
+  }
+}
+
+function handleEnderecoSave() {
+  const endereco = {
+    cep: document.getElementById("cep").value,
+    logradouro: document.getElementById("logradouro").value,
+    complemento: document.getElementById("complemento").value,
+    bairro: document.getElementById("bairro").value,
+    localidade: document.getElementById("localidade").value,
+    uf: document.getElementById("uf").value,
+    numero: document.getElementById("numero").value,
+    principal: false,
+    tipo: 0,
+  };
+
+  const validacaoErro = validarEndereco(endereco);
+  if (validacaoErro) {
+    Swal.fire("Erro!", validacaoErro, "warning");
+    return;
+  }
+  salvarNovoEndereco(endereco);
+}
+
+async function salvarNovoEndereco(endereco) {
+  try {
+    const response = await fetch(`http://localhost:8080/cliente/add/endereco`, {
+      method: "POST",
+      body: JSON.stringify(endereco),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    });
+    const data = await response.json();
+    if (data.message === "success") {
+      Swal.fire("", "Endereço cadastrado com sucesso.", "success").then(() => {
+        location.reload();
+      });
+    } else {
+      Swal.fire(
+        "Erro!",
+        "Erro ao cadastrar novo endereço! Tente novamente mais tarde.",
+        "error"
+      );
+    }
+  } catch (error) {
+    Swal.fire(
+      "Erro!",
+      "Erro ao cadastrar novo endereço! Tente novamente mais tarde.",
+      "error"
+    );
+  }
+}
+
+function validarEndereco(endereco) {
+  if (!endereco.cep || endereco.cep.length !== 9) return "CEP é obrigatório!";
+  if (!endereco.logradouro) return "Logradouro é obrigatório!";
+  if (!endereco.bairro) return "Bairro é obrigatório!";
+  if (!endereco.localidade) return "Cidade é obrigatória!";
+  if (!endereco.uf || endereco.uf.length !== 2) return "UF inválida!";
+  if (!endereco.numero) return "Número é obrigatório!";
+  return null;
+}
+
+const addEventListeners = () => {
+  document.getElementById("cep").addEventListener("blur", handleCEPBlur());
+};
+
+const handleCEPBlur = () => (e) => {
+  if (e.target.value.length == 9) {
+    fetchAddress();
+  }
+};
+
+function alterAllInputsClass(invert = false) {
+  const div = document.getElementById(`deliveryAddressFieldsInfo`);
+  const inputs = div.querySelectorAll("input");
+  const pElements = div.querySelectorAll("p");
+
+  if (invert) {
+    inputs.forEach((input) => {
+      input.classList.remove("has-ok");
+      input.classList.add("has-error");
+    });
+    return;
+  }
+  pElements.forEach((p) => {
+    p.remove();
+  });
+  inputs.forEach((input) => {
+    if (input.value.trim()) {
+      input.classList.remove("has-error");
+      input.classList.add("has-ok");
+    }
+  });
+}
+
+const fetchAddress = () => {
+  let cep = document.getElementById(`cep`);
+  let addressFields = document.getElementById(`deliveryAddressFieldsInfo`);
+  const existingError = cep.nextElementSibling;
+  if (existingError && existingError.classList.contains("error-message")) {
+    existingError.textContent = "";
+  }
+  fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.erro) {
+        alterAllInputsClass(true);
+        addressFields.classList.remove("hidden");
+        cep.classList.remove("has-error");
+        alert("CEP não encontrado!");
+      } else {
+        document.getElementById(`logradouro`).value = data.logradouro;
+        document.getElementById(`bairro`).value = data.bairro;
+        document.getElementById(`localidade`).value = data.localidade;
+        document.getElementById(`uf`).value = data.uf;
+        cep.classList.remove("has-error");
+        cep.classList.add("has-ok");
+        alterAllInputsClass();
+        addressFields.classList.remove("hidden");
+      }
+    })
+    .catch(() => {
+      alterAllInputsClass(true);
+      addressFields.classList.remove("hidden");
+      alert("Erro ao buscar o CEP!");
+    });
+};
+
+var pedido = {
+  clienteID: "",
+  enderecoID: "",
+  valorTotal: "",
+  metodoPagamento: "",
+  items: [],
+  status: "",
+};
+
+function app() {
+  return {
+    step: 1,
+
+    goToNextStep() {
+      if (this.validateStep(this.step)) {
+        this.step++;
+      }
+    },
+
+    validateStep(step) {
+      switch (step) {
+        case 1:
+          return this.validateStep1();
+        case 2:
+          return this.validateStep2();
+        default:
+          return true;
+      }
+    },
+
+    setErrorMessage: (input, message) => {
+      if (message) {
+        input.classList.remove("has-ok");
+        input.classList.add("has-error");
+      } else {
+        input.classList.remove("has-error");
+        input.classList.add("has-ok");
+      }
+
+      const existingError = input.nextElementSibling;
+
+      if (existingError && existingError.classList.contains("error-message")) {
+        existingError.textContent = message;
+      } else {
+        const p = document.createElement("p");
+        p.classList.add("error-message");
+        p.innerText = message;
+        input.insertAdjacentElement("afterend", p);
+      }
+    },
+    regexNumeroCpf: (dado) => {
+      dado = dado.replace(/[^\d]+/g, "");
+      return dado;
+    },
+
+    validateStep1: function () {
+      let result = null;
+      var addressItems = document.querySelectorAll(
+        "#address-list .address-item"
+      );
+
+      for (let i = 0; i < addressItems.length; i++) {
+        if (addressItems[i].classList.contains("selected")) {
+          result = addressItems[i].getAttribute("data-id");
+          pedido.enderecoID = result;
+        }
+      }
+
+      if (result === null) alert("Selecione um endereço de entrega!");
+      // this.setErrorMessage(name, "Nome é obrigatório!", "red");
+
+      console.info(pedido);
+      return result >= 0;
+    },
+
+    validateStep2: function () {
+      resetMessages();
+      let formaPag = {
+        "payment-card": "CARTAO",
+        pix: "PIX",
+        beleto: "BOLETO",
+      };
+      let isValid = true;
+      let result = null;
+      let pagamentoItems = document.querySelectorAll(
+        "#payment-list .payment-item"
+      );
+
+      for (let i = 0; i < pagamentoItems.length; i++) {
+        if (pagamentoItems[i].classList.contains("selected")) {
+          result = pagamentoItems[i].id;
+        }
+      }
+
+      if (result === null) {
+        alert("Selecione um metodo de pagamento!");
+        return false;
+      }
+
+      if (result === "payment-card") {
+        // Validação do nome no cartão
+        const cardHolder = document.getElementById("card-holder");
+
+        if (cardHolder.value.trim() === "") {
+          showError(
+            "card-holder",
+            "Por favor, insira o nome impresso no cartão."
+          );
+          isValid = false;
+        }
+
+        // Validação do número do cartão
+        const cardNo = document.getElementById("card-no");
+        cardNo.value = cardNo.value.replace(/[^\d]+/g, "");
+        if (!validateCardNumber(cardNo.value)) {
+          showError("card-no", "Número do cartão inválido.");
+          isValid = false;
+        }
+
+        // Validação da data de vencimento
+        const expiryDate = document.getElementById("data-vencimento");
+        if (!validateExpiryDate(expiryDate.value)) {
+          showError(
+            "data-vencimento",
+            "Data de vencimento inválida ou expirada."
+          );
+          isValid = false;
+        }
+
+        // Validação do CVC
+        const cvc = document.getElementById("codigo-cvc");
+        if (!validateCVC(cvc.value)) {
+          showError("codigo-cvc", "Código CVC inválido.");
+          isValid = false;
+        }
+      }
+
+      pedido.metodoPagamento = formaPag[result.toLowerCase()];
+      console.info(pedido);
+      return result !== null && isValid;
+    },
+
+    submitCompra: function () {
+      document.getElementById("loader").style.display = "block";
+      document.getElementById("btnComprar").disabled = true;
+      document.getElementById("btnVolta").disabled = true;
+
+      makeRequest(
+        "POST",
+        "/pedido/novo-pedido",
+        pedido,
+        "Compra efetuada com sucesso"
+      );
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("btnComprar").disabled = false;
+      document.getElementById("btnVolta").disabled = false;
+    },
+  };
+}
+
+function validateCardNumber(number) {
+  // Uma simples checagem para ver se o número contém 16 dígitos (Você pode expandir com expressões regulares para um padrão específico)
+  return /^\d{16}$/.test(number.replace(/\s+/g, ""));
+}
+
+function validateExpiryDate(date) {
+  // Checagem se a data está no formato AAAA-MM e se é uma data futura
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1; // getMonth retorna 0-11
+  const [year, month] = date.split("-").map((part) => parseInt(part, 10));
+
+  return year > currentYear || (year === currentYear && month >= currentMonth);
+}
+
+function validateCVC(cvc) {
+  // Checagem se o CVC contém 3 ou 4 dígitos
+  return /^\d{3,4}$/.test(cvc);
+}
+
+function showError(inputId, message) {
+  const inputElement = document.getElementById(inputId);
+  inputElement.classList.add("border-red-500"); // Adiciona uma borda vermelha
+
+  const errorDiv = document.createElement("div");
+  errorDiv.textContent = message;
+  errorDiv.classList.add("text-red-500", "text-sm", "mt-1"); // Classe de erro para a mensagem
+  inputElement.parentNode.insertBefore(errorDiv, inputElement.nextSibling);
+}
+
+function resetMessages() {
+  // Remove as mensagens de erro anteriores e as bordas vermelhas
+  const errorMessages = document.querySelectorAll(".text-red-500");
+  errorMessages.forEach((message) => message.remove());
+
+  const errorInputs = document.querySelectorAll(".border-red-500");
+  errorInputs.forEach((input) => input.classList.remove("border-red-500"));
+}
+
+function makeRequest(e, t, a, i) {
+  console.log(a),
+    $.ajax({
+      type: e,
+      url: `http://localhost:8080${t}`,
+      data: JSON.stringify(a),
+      contentType: "application/json; charset=utf-8",
+      success: function (e) {
+        [200, 201].includes(e.status) && "sucess" === e.message
+          ? Swal.fire({
+              icon: "success",
+              title: i,
+              showConfirmButton: true,
+              confirmButtonText: "OK",
+            }).then((result) => {
+              var inputs = document.querySelectorAll("input, textarea");
+              inputs.forEach(function (input) {
+                input.value = "";
+              });
+              limpaCarrinho();
+
+              window.location.href = "/meus-pedidos";
+            })
+          : Swal.fire({
+              icon: "error",
+              title: "Erro",
+              text: "Resposta inesperada do servidor. Por favor, tente novamente.",
+            });
+      },
+      error: function (e) {
+        let t = e.responseJSON,
+          a = "<br/><br/>"; // Iniciei a variável com as quebras de linha
+
+        if (t.details) {
+          a += t.details.map((detail) => "-" + detail).join("<br/>"); // Adicionando detalhes com quebras de linha
+        }
+
+        Swal.fire({
+          icon: "error",
+          title: t.message, // Definindo t.message como o título
+          html: a, // Configurando o conteúdo formatado em HTML como o corpo da mensagem
+        });
+      },
+    });
+}
+
+window.onload = function () {
+  var inputs = document.querySelectorAll("input, textarea");
+  inputs.forEach(function (input) {
+    input.value = "";
+  });
+};
